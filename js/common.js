ece7e30d3a86a83eb628172c09785bc12e54afdd
Damir Khavaza <damir.redmouse@gmail.com>
2018-04-23 00:10:11 +0600

    test1

---
+++ js/common.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+
+
+$(".top_phone .wrapper .tab").click(function() {
+	$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+	$(".s2-item").equalHeights();
+	$(".s2-item .img-wrap").equalHeights();
+
+
+	$(".preim").waypoint(function() {
+		$(".s2-item").each(function(index) {
+			var ths = $(this);
+			setInterval(function() {
+				ths.addClass("on");
+			}, 200*index);
+		});
+	}, {
+		offset : "30%"
+	});
+
+$('.owl-carousel').owlCarousel({
+      loop:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true   
+        }
+    },
+    navText : ""
+});
+
+var yWidgetSettings ={
+	buttonColor : '#8238A7'
+};
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+
+	$(".arrow-bottom").click(function() {
+		$("html, body").animate({ scrollTop: $(".main_head").height()+30 }, "slow");
+		return false;
+	});
+
+
+	$(".section_header ").animated("fadeInUp", "fadeOutDown");
+$(".map ").animated("zoomIn", "zoomOut");
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+	$(".top_mnu ul a").mPageScroll2id();
+
+});
+
+
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h2").animated("fadeInDown", "fadeOutUp");
+	$(".top_text h1").animated("fadeInUp", "fadeOutDown");
+
+
+
+
+}); 
